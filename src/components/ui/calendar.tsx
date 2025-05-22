02a634f1ba40160e578136b4c4b886902b1339f7
Xncoder <xncoder2003@gmail.com>
2025-05-22 05:44:50 +0000

    remove from dropdown "Month:" and "Year:"

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -55,12 +55,8 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ className, ...props }) => (
-          <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
-        ),
-        IconRight: ({ className, ...props }) => (
-          <ChevronRight className={cn("h-4 w-4", className)} {...props} />
-        ),
+        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
+        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
       }}
       {...props}
     />
